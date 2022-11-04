933c0f0f01cdbb28c1f85927123812cdb3882ac7
fortune710 <fortune0208@yahoo.com>
2022-11-04 09:45:31 +0100

    added type prop

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,12 +1,18 @@
+import styles from './button.module.scss'
+
 interface ButtonProps {
+    type?: 'button'|'submit'|'reset'
     text: string,
     className?: string,
     onClick?: React.MouseEventHandler<HTMLButtonElement>,
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, className, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ text, className, onClick, type }) => {
     return(
-        <button className={`${className}`} onClick={onClick}>
+        <button 
+            type={type}
+            className={`${className} ${styles.button}`} 
+            onClick={onClick}>
             {text}
         </button>
     )
