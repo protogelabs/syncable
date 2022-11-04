afb65d099178022cfbdefc6101ccb28eebd58b15
fortune710 <fortune0208@yahoo.com>
2022-11-04 09:42:57 +0100

    fixed the overflow caused by the circles

--- src/components/Circles/circles.tsx
+++ src/components/Circles/circles.tsx
@@ -7,7 +7,9 @@ type BackgroundCirclesProps = {
 export const BackgroundCircles: React.FC<BackgroundCirclesProps> = ({ children }) => {
     return(
         <main className={styles.container}>
+
             { children }
+            
             <div className={styles.circle3} />
             <div className={styles.circle1}/>
             <div className={styles.circle2}/>
