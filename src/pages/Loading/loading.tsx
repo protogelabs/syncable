72dac2987242142ccafafb9e4c156cd72bedd98e
fortune710 <fortune0208@yahoo.com>
2022-11-05 13:18:50 +0100

    loading page created

---
+++ src/pages/Loading/loading.tsx
@@ -0,0 +1,20 @@
+import styles from './loading.module.scss'
+import logo from '../../assets/loading.svg'
+
+export const Loading: any = () => {
+  return (
+    <div className={styles.textcolor} >
+
+
+      <img src={logo} className={styles.img} alt="t" />
+      Syncable
+
+      <div className={styles.load}>
+
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+      </div>
+    </div>
+  )
+}
