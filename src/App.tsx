fd3710d0f829cdbd20950f9b24b380221be4be14
fortune710 <fortune0208@yahoo.com>
2022-11-05 13:10:42 +0100

    added route for onboarding page

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { BackgroundCircles, Button,Words } from './components'
 
 import { useTheme } from './hooks/useTheme'
 
+import { Routes, Route } from 'react-router-dom';
+
+//Page Imports
+import { OnboardingPage } from './pages';
+
 function App() {
   const [resolvedTheme, toggleTheme] = useTheme('dark')
 
   return (
-    <BackgroundCircles>
-
-      <Words/>
-    </BackgroundCircles>
+    <Routes>
+      <Route path="/" element={<OnboardingPage/>} />
+    </Routes>
   )
 }
 
