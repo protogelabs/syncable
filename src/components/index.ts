d8cfdd95f51d2aca67c99c1eef253b55bab64f11
raji2004 <kingraj1344@gmail.com>
2022-11-22 10:25:18 +0100

    fix conflicts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,4 @@
 export * from './Button/button';
 export * from './Circles/circles';
-<<<<<<< HEAD
-export * from './Words/words';
 export * from './Tabs/tabs';
-=======
->>>>>>> 560dbc4f276c212adbc53f1b763860f9e720b470
+
