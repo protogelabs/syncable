67e4fc9a390abbf0822a42e80ee5c9fb9d2ff7d1
fortune710 <fortune0208@yahoo.com>
2022-11-23 16:02:24 +0100

    added segmetn to home page

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,7 +1,13 @@
 import { AppHeader, Ongoingclass } from "../../components";
 import avatar from '../../assets/pfp.png';
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import styles from './home.module.scss';
 
 export const HomePage: React.FC = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
     return(
         <main>
             <AppHeader avatarSrc={avatar} />
@@ -11,6 +17,20 @@ export const HomePage: React.FC = () => {
                 theme="orange"
                 location="online"
             />
+
+            <div className={styles.segment}>
+                <span 
+                    onClick={() => navigate('/main/home/class')}
+                    className={`flex-center ${location.pathname === '/main/home/class' ? `${styles.active}` : null}`}>
+                    Class
+                </span>
+                <span 
+                    onClick={() => navigate('/main/home/time-table')}
+                    className={`flex-center ${location.pathname === '/main/home/time-table' ? `${styles.active}` : null}`}>
+                    Timetable
+                </span>
+            </div>
+            <Outlet/>
 
         </main>
     )
