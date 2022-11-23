326331cc2249a32a3e028b32697a7aa195842af2
fortune710 <fortune0208@yahoo.com>
2022-11-23 15:13:09 +0100

    created home page

--- src/pages/Main/main.tsx
+++ src/pages/Main/main.tsx
@@ -1,18 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { Tabs ,Pfp,Ongoingclass} from "../../components";
+import { Tabs } from "../../components";
 
 export const MainLayout: React.FC = () => {
     
     
     return(
         <>
-           <Pfp />
-            <Ongoingclass
-            coursecode= 'sen201'
-            course= 'intro to software engineering'
-            theme="orange"
-            location="online"
-            />
             <Outlet/>
             <Tabs/>
         </>
